12f2c74c3bf3a824efbbcbaaf68eb3d47c74477c
Angelina Nikiforova <anikifor@redhat.com>
2022-01-02 14:30:41 +0100

    makemigrations in entrypoint, at least for the initial setup

--- home_inventory/entrypoint.sh
+++ home_inventory/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+python manage.py makemigrations inventory
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 uwsgi --socket :8000 --module home_inventory.wsgi
