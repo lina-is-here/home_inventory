332049c41ddc25541d2a6ebb1c174f0eb0ec6f48
Angelina Nikiforova <anikifor@redhat.com>
2022-01-05 16:23:06 +0100

    commit migrations and remove makemigrations from entrypoint

--- home_inventory/entrypoint.sh
+++ home_inventory/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-python manage.py makemigrations inventory
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 uwsgi --socket :8000 --module home_inventory.wsgi
