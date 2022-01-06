702eb12aaf7ea5d78fe63564aaef583d064eafb5
Angelina Nikiforova <anikifor@redhat.com>
2022-01-06 20:56:35 +0100

    style forms

--- home_inventory/inventory/forms.py
+++ home_inventory/inventory/forms.py
@@ -1,3 +1,5 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 from dal import autocomplete
 from django.forms import ModelForm, DateInput, ModelChoiceField
 from django.utils.translation import gettext, gettext_lazy as _
@@ -5,6 +7,14 @@ from .models import Item, Category, Product, Location
 
 
 class LocationForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = 'add-form'
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Submit'))
+
     class Meta:
         model = Location
         fields = "__all__"
@@ -15,14 +25,16 @@ class ItemForm(ModelForm):
     category = ModelChoiceField(
         label=_("Category"),
         queryset=Category.objects.all(),
-        widget=autocomplete.ModelSelect2(url="category-autocomplete"),
+        widget=autocomplete.ModelSelect2(url="category-autocomplete",
+                                         attrs={'data-width': '100%'}),
     )
 
     class Meta:
         model = Item
         fields = "__all__"
         widgets = {
-            "name": autocomplete.ModelSelect2(url="product-autocomplete"),
+            "name": autocomplete.ModelSelect2(url="product-autocomplete",
+                                              attrs={'data-width': '100%'}),
             "expiry_date": DateInput(
                 format="%Y-%m-%d",
                 attrs={
@@ -32,6 +44,14 @@ class ItemForm(ModelForm):
                 },
             ),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = 'add-form'
+        self.helper.form_class = 'form-horizontal'
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Submit'))
 
     def save(self, commit=True):
         # assign category to product or change the existing one
