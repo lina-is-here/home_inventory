743f521972c4f6fcbe89d65f6b76a645ad9070bb
Angelina Nikiforova <anikifor@redhat.com>
2022-01-08 09:15:42 +0100

    fix uWSGI warnings

--- home_inventory/entrypoint.sh
+++ home_inventory/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
-uwsgi --socket :8000 --module home_inventory.wsgi
+uwsgi --socket :8000 --uid=1001 --gid=1001 --master --module home_inventory.wsgi --vacuum
