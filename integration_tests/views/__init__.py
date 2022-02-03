f02522f50686c793a3114ed05c069524c86483b8
Angelina Nikiforova <anikifor@redhat.com>
2022-02-03 09:52:36 +0100

    add bootsrtap button widget and add_location_button to index view

--- integration_tests/views/__init__.py
+++ integration_tests/views/__init__.py
@@ -2,10 +2,14 @@ from navmazing import Navigate
 from widgetastic.widget import View, Text
 
 from base.browser import HI_UI, HINavigateStep
+from base.widgetastic_widgets import Button
 
 
 class BasePageView(View):
     title = Text(locator=".//title")
+    # TODO: navbar and footer are not Text widgets
+    navbar = Text(locator=".//nav")
+    footer = Text(locator=".//footer")
 
     @property
     def is_displayed(self):
@@ -13,9 +17,19 @@ class BasePageView(View):
 
 
 class IndexPageView(BasePageView):
+    add_location_button = Button(locator=".//a[@id='add-location-button']")
+
     @property
     def is_displayed(self):
-        return self.title.text == "Home Inventory"
+        return (
+            self.title.text == "Home Inventory"
+            and self.add_location_button.is_displayed
+        )
+
+    def add_location(self, location_name):
+        """Add location from index page"""
+        self.add_location_button.click()
+        # TODO: finish this method
 
 
 # ------------------Navigation---------------------------#
